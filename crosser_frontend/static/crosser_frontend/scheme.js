f56673010a5d24ab25d6a8cb8935386b2a14de51
James Southern <jms301@gmail.com>
2013-07-23 18:38:56 +0100

    sortof working version of add loci in cross list (displaying not actually adding)

--- crosser_frontend/static/crosser_frontend/scheme.js
+++ crosser_frontend/static/crosser_frontend/scheme.js
@@ -1,4 +1,4 @@
-angular.module('scheme', ['schemecon', 'crosserFilters']).
+angular.module('scheme', ['ui.bootstrap', 'schemecon', 'crosserFilters']).
     config(function($interpolateProvider){
         $interpolateProvider.startSymbol('{[{').endSymbol('}]}')
     });
@@ -9,38 +9,47 @@ function PlanCtrl($scope, Scheme, Plant, Cross, Locus, Species, $location) {
     // set up the PlanController context
     $scope.plan_id = window.location.pathname.split("/")[2]; 
 
+    $scope.get_plant_by_uri = function (ref) { 
+        return _.findWhere($scope.scheme.plants, {resource_uri: ref});
+    };
+
+    $scope.get_cross_by_uri = function (ref) {
+        
+        return _.findWhere($scope.scheme.crosses, {resource_uri: ref});
+    };
+
     $scope.add_parents = function(c, index, crosses) {
-            if (c.left_plant_parent != null)
-                c.left_parent = c.left_plant_parent;
-            else
-                c.left_parent = c.left_cross_parent;
-    
-            if (c.right_plant_parent != null)
-                c.right_parent = c.right_plant_parent;
-            else
-                c.right_parent = c.right_cross_parent;
-            
-        };
-    
-   $scope.strip_parents = function(c, index, crosses) {
-            if (c.left_parent != null) {
-                if (c.left_parent.indexOf('/api/v1/cross/')==0)
-                    c.left_cross_parent = c.left_parent
-                else if (c.left_parent.indexOf('/api/v1/plant/')==0)
-                    c.left_plant_parent = c.left_parent
+        if (c.left_plant_parent != null)
+            c.left_parent = c.left_plant_parent;
+        else
+            c.left_parent = c.left_cross_parent;
+   
+        if (c.right_plant_parent != null)
+            c.right_parent = c.right_plant_parent;
+        else
+            c.right_parent = c.right_cross_parent;
+    };
 
-            }
-            if (c.right_parent != null) {
-                if (c.right_parent.indexOf('/api/v1/cross/')==0)
-                    c.right_cross_parent = c.right_parent
-                else if (c.right_parent.indexOf('/api/v1/plant/')==0)
-                    c.right_plant_parent = c.right_parent
+ 
+    $scope.strip_parents = function(c, index, crosses) {
+        if (c.left_parent != null) {
+            if (c.left_parent.indexOf('/api/v1/cross/')==0)
+                c.left_cross_parent = c.left_parent
+            else if (c.left_parent.indexOf('/api/v1/plant/')==0)
+                c.left_plant_parent = c.left_parent
 
-            }
+        }
+        if (c.right_parent != null) {
+            if (c.right_parent.indexOf('/api/v1/cross/')==0)
+                c.right_cross_parent = c.right_parent
+            else if (c.right_parent.indexOf('/api/v1/plant/')==0)
+                c.right_plant_parent = c.right_parent
 
-            delete c.left_parent 
-            delete c.right_parent 
-        };
+        }
+
+        delete c.left_parent 
+        delete c.right_parent 
+    };
 
 
     $scope.scheme = Scheme.get({id: $scope.plan_id}, function () {
@@ -48,6 +57,8 @@ function PlanCtrl($scope, Scheme, Plant, Cross, Locus, Species, $location) {
         // this allows us to use convenient ng-options stuff in the frontend
         // the properties will be split out before saving.
         _.each($scope.scheme.crosses, $scope.add_parents);
+        // setup an array 'ancestor_loci' in each cross so we can tell which loci are available to add into any cross. 
+        _.each($scope.scheme.crosses, $scope.add_loci_list);
 
         // setup array 'parents' that can be used to populate select options 
         // the type field is used to provide groupings in the dropdown
@@ -62,7 +73,9 @@ function PlanCtrl($scope, Scheme, Plant, Cross, Locus, Species, $location) {
                     resource_uri: cross.resource_uri};
         });
         $scope.parents = tmp_cross.concat(tmp_plants); 
+
     });
+ 
 
     $scope.species = Species.get(function () {
         _.each($scope.species.objects, function(element, index, list) { 
@@ -72,8 +85,40 @@ function PlanCtrl($scope, Scheme, Plant, Cross, Locus, Species, $location) {
     $scope.to_del = [];
     });
 
+    // function to get all origin plants for any cross
+    $scope.add_loci_list = function(cross, index, crosses) {
+        plants = [];
+        parents = [];
+        loci = [];
+
+        parents.push(cross.left_parent);
+        parents.push(cross.right_parent);
+
+        while(parents.length != 0) { 
+            process = parents.pop();
+            //console.log(process);
+            if (process.indexOf('/api/v1/cross/')==0) { 
+                c = $scope.get_cross_by_uri(process);
+                //console.log(process);
+                //console.log(c);
+                parents.push(c.right_parent);
+                parents.push(c.left_parent);
+            } else if (process.indexOf('/api/v1/plant/')==0) {
+                plants.push($scope.get_plant_by_uri(process));
+            }
+        }
+        console.log(plants);
+        cross.ancestor_loci = _.flatten(_.map(_.uniq(plants), 
+        function(plant, index, plants) { 
+            return plant.loci
+        }));
+    };
+
+    $scope.strip_loci_list = function(cross, index, crosses) {
+        delete cross.ancestor_loci; 
+    };
     // function to add a new locus to a plant
-    $scope.addLocus =   function ( plant ) {
+    $scope.add_locus =   function ( plant ) {
         plant.loci.push({"name": null, "locus_type": null, "linkageGroup": null});
     };
     // function to remove a locus from the scheme & any crosses
@@ -85,10 +130,17 @@ function PlanCtrl($scope, Scheme, Plant, Cross, Locus, Species, $location) {
         });
 
         plant.loci = _.reject(plant.loci, function (item){ 
-            return item.resource_uri == locus.resource_uri;
+            return item === locus; // have to match exactly otherwise we remove things with a blank resource_uri.
         }); 
         $scope.to_del.push(locus.resource_uri);
     };
+
+    // function to remvoe a locus from a cross
+    $scope.removeCrossLocus = function (cross, locus) {
+        cross.loci = _.reject(cross.loci, function(l){
+            return locus == l;
+        });
+    }
 
     // function to remove a cross from the scheme & parent array
     $scope.removeCross = function (cross) {
@@ -121,7 +173,9 @@ function PlanCtrl($scope, Scheme, Plant, Cross, Locus, Species, $location) {
     // update function 
     $scope.updateScheme = function ( scheme, to_del ) {
         _.each(scheme.crosses, $scope.strip_parents);
+        _.each(scheme.crosses, $scope.strip_loci_list);
         scheme.update(scheme);
+        _.each(scheme.crosses, $scope._loci_list);
         _.each(scheme.crosses, $scope.add_parents);
     };
 }
