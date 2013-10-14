1d5f28c1413bbe39aee54c0ae5aa006955f63397
James Southern <jms301@gmail.com>
2013-10-14 15:36:59 +0100

    add in non-2way-relation versions of plant/cross etc to speed mass save of scheme

--- crosser_frontend/api.py
+++ crosser_frontend/api.py
@@ -23,11 +23,11 @@ class SpeciesResource(ModelResource):
 class SchemeResource(ModelResource):
     species = fields.ForeignKey(SpeciesResource, 'species', null=True)
     owner = fields.ForeignKey(UserResource, 'owner')    
-
+ 
     system = fields.ToOneField('crosser_frontend.api.SystemResource', 'system', full=True, related_name='scheme', null=True)
-    plants = fields.ToManyField('crosser_frontend.api.PlantResource', 'plants', full=True, related_name='scheme', null=True)
-    crosses = fields.ToManyField('crosser_frontend.api.CrossResource', 'crosses', full=True, related_name='scheme', null=True)
-    outputs = fields.ToManyField('crosser_frontend.api.OutputResource', 
+    plants = fields.ToManyField('crosser_frontend.api.SPlantResource', 'plants', full=True, related_name='scheme', null=True)
+    crosses = fields.ToManyField('crosser_frontend.api.SCrossResource', 'crosses', full=True, related_name='scheme', null=True)
+    outputs = fields.ToManyField('crosser_frontend.api.SOutputResource', 
         'outputs', full=True, related_name='scheme', null=True)
 
     class Meta:
@@ -39,7 +39,6 @@ class SchemeResource(ModelResource):
 
 class SystemResource(ModelResource):
     owner = fields.ForeignKey(UserResource, 'owner')    
-    scheme = fields.ForeignKey(SchemeResource, 'scheme')
 
     class Meta:
         queryset = System.objects.filter(frozen=False)
@@ -48,6 +47,15 @@ class SystemResource(ModelResource):
         resource_name = 'system'
         always_return_data=True
 
+class SOutputResource(ModelResource):
+    owner = fields.ForeignKey(UserResource, 'owner')    
+
+    class Meta: 
+        queryset = Output.objects.filter(frozen=False)
+        authentication = SessionAuthentication()
+        authorization = UserAuthorization()
+        resource_name = 'output'
+        always_return_data=True
 
 class OutputResource(ModelResource):
     owner = fields.ForeignKey(UserResource, 'owner')    
@@ -60,16 +68,40 @@ class OutputResource(ModelResource):
         resource_name = 'output'
         always_return_data=True
 
-class PlantResource(ModelResource):
+#Dupe of the plant resource to allow faster saving of whole schemes.
+class SPlantResource(ModelResource):
     owner = fields.ForeignKey(UserResource, 'owner')    
     loci = fields.ToManyField('crosser_frontend.api.LocusResource', 'loci', full=True, related_name='plant', null=True)
-    scheme = fields.ForeignKey(SchemeResource, 'scheme')
     class Meta:
         queryset = Plant.objects.filter(frozen=False)
         authentication = SessionAuthentication()
         authorization = UserAuthorization()
         resource_name = 'plant'
         always_return_data=True
+
+class PlantResource(ModelResource):
+    owner = fields.ForeignKey(UserResource, 'owner')    
+    loci = fields.ToManyField('crosser_frontend.api.LocusResource', 'loci', full=True, related_name='plant', null=True)
+    scheme = fields.ForeignKey(SchemeResource, 'scheme')
+
+    class Meta:
+        queryset = Plant.objects.filter(frozen=False)
+        authentication = SessionAuthentication()
+        authorization = UserAuthorization()
+        resource_name = 'plant'
+        always_return_data=True
+
+#Dupe of the locus resource to allow faster saving of whole schemes.
+class SLocusResource(ModelResource): 
+    owner = fields.ForeignKey(UserResource, 'owner')    
+
+    class Meta:
+        queryset = Locus.objects.filter(frozen=False)
+        authentication = SessionAuthentication()
+        authorization = UserAuthorization()
+        resource_name = 'locus'
+        always_return_data=True
+
 
 class LocusResource(ModelResource): 
     owner = fields.ForeignKey(UserResource, 'owner')    
@@ -81,6 +113,25 @@ class LocusResource(ModelResource):
         authorization = UserAuthorization()
         resource_name = 'locus'
         always_return_data=True
+
+#Dupe of the cross resource to allow faster saving of whole schemes.
+class SCrossResource(ModelResource):
+    owner = fields.ForeignKey(UserResource, 'owner')    
+    loci = fields.ToManyField('crosser_frontend.api.LocusResource', 'loci', null=True)
+    left_plant_parent = fields.ForeignKey(PlantResource, 'left_plant_parent', null=True, blank=True)
+    right_plant_parent = fields.ForeignKey(PlantResource, 'right_plant_parent', null=True, blank=True)
+
+    left_cross_parent = fields.ForeignKey('self', 'left_cross_parent', null=True, blank=True)
+
+    right_cross_parent = fields.ForeignKey('self', 'right_cross_parent', null=True, blank=True)
+    
+    class Meta: 
+        queryset = Cross.objects.filter(frozen=False)
+        authentication = SessionAuthentication()
+        authorization = UserAuthorization()
+        resource_name = 'cross'
+        always_return_data=True
+
 
 class CrossResource(ModelResource):
     owner = fields.ForeignKey(UserResource, 'owner')    
