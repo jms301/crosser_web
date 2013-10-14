7d0cb1662454cd001ab3f862cd7f14703f3e8cb8
James Southern <jms301@gmail.com>
2013-10-14 11:26:43 +0100

    raise errors to kill task on failure, show logs on failure, limit to single worker

--- crosser_frontend/tasks.py
+++ crosser_frontend/tasks.py
@@ -1,7 +1,10 @@
 import subprocess
 import datetime
 from celery import task
- 
+from celery.utils.log import get_task_logger 
+
+logger = get_task_logger(__name__)
+
 @task(name='crosser_frontend.tasks.process_scheme')
 def process_scheme(calc_id): 
     calc = Calculation.objects.get(pk=calc_id)
@@ -23,11 +26,23 @@ def process_scheme(calc_id):
     out, err = p.communicate()   
     ret = p.wait()
 
+    logger.debug('crosser return code')
+    logger.debug(ret)
+    if ret != 0:
+        logger.debug('raising')
+        raise RuntimeError('Crosser failed to execute')
+
     cmd = ["./crosser/runR.RScript", output_dir]
     p = subprocess.Popen(cmd, stdout=subprocess.PIPE,stderr=subprocess.PIPE)
 
     out, err = p.communicate()   
     ret = p.wait()
+
+    logger.debug('Rscript return code')
+    logger.debug(ret)
+    if ret != 0:
+        logger.debug('raising')
+        raise RuntimeError('Rscript failed to exec')
 
     del calc
     calc = Calculation.objects.get(pk=calc_id)
